34e73a5b0ead92fcefe53d337804d73c2c48cf3b
dx <bumonkey@163.com>
2020-04-28 11:40:30 +0800

    初始化

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,23 +1,11 @@
 package com.example.demo;
 
-import com.example.demo.dao.MenuDao;
-import com.example.demo.entity.Menu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
-import java.util.List;
-
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Resource
-	private MenuDao menuDao;
-
-
 	@Test
 	void contextLoads() {
-
 	}
 
 }
